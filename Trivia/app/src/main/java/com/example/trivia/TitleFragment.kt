c5155f7a5284e3cbdcc7654e4d617b4e17df50d2
Sergio <chencho.bryant@gmail.com>
2019-03-10 19:43:37 +0100

    Trivia project to play with Fragments

---
+++ Trivia/app/src/main/java/com/example/trivia/TitleFragment.kt
@@ -0,0 +1,28 @@
+package com.example.trivia
+
+
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.trivia.databinding.FragmentTitleBinding
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_title, container, false)
+
+        return binding.root
+    }
+
+
+}
